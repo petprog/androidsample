e7a1bf73e80eb95ff656e532dcf9521fb4e0dce0
petprog <farinupeter@gmail.com>
2018-08-28 04:59:31 +0100

    add findViewById

--- app/src/main/java/com/tableboy/gittutorial/MainActivity.kt
+++ app/src/main/java/com/tableboy/gittutorial/MainActivity.kt
@@ -2,11 +2,17 @@ package com.tableboy.gittutorial
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mTitle : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mTitle = findViewById(R.id.title)
     }
 }
